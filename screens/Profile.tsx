4e9663c9a6bd4e434e4c3e063e007a7df14cb939
Peter van Dijk <peter.van.dijk@bynder.com>
2021-03-02 22:46:22 +0100

    with logout

--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -1,12 +1,31 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
+import { useApolloContext } from '../components/ApolloProviderWrapper';
+
 
 import { Text, View } from '../components/Themed';
 
 export default function Profile() {
+  const { setLocalToken } = useApolloContext();
+
+  const removeToken = async () => {
+    setLocalToken(null);
+    return await AsyncStorage.removeItem('token');
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Profile</Text>
+      <Button
+        disabled={false}
+        mode="contained"
+        loading={false}
+        onPress={removeToken}
+        accessibilityLabel="LogOut"
+      >
+        Log out
+        </Button>
     </View>
   );
 }
