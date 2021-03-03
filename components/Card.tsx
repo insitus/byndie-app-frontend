81fe761773297071f6f65b85411aedb06f30e5fa
Brian Vollebergh <brian.vollebergh@bynder.com>
2021-03-03 03:07:11 +0100

    Listing page improvement, Card component for event data

---
+++ components/Card.tsx
@@ -0,0 +1,98 @@
+import * as React from "react";
+import { Card } from "react-native-paper";
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+  margin-bottom: 28px;
+  font-family: "Arial";
+  border: 2px solid rgba(0, 34, 51, 0.1);
+  border-radius: 5px;
+`;
+
+const EventDetails = styled.div`
+  padding: 14px 20px;
+`;
+
+const Title = styled.div`
+  margin-bottom: 8px;
+  font-size: 20px;
+  overflow: hidden;
+  word-break: break-all;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+const Location = styled.div`
+  margin-bottom: 12px;
+  font-size: 17px;
+`;
+
+const Description = styled.div`
+  font-size: 14px;
+`;
+
+const Details = styled.div`
+  margin-bottom: 12px;
+  font-size: 14px;
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+const Date = styled.div`
+  font-size: 14px;
+  font-weight: 600;
+`;
+
+const Host = styled.div`
+  font-size: 14px;
+  font-weight: 600;
+  margin-bottom: 12px;
+`;
+
+const Reviews = styled.div`
+  font-size: 12px;
+`;
+
+export default function EventCard() {
+  // Data for testing purposes, actual props can be passed to this component
+  const props = {
+    name: "Tour of Rotterdam - Kop van Zuid",
+    eventType: "City tour",
+    country: "Netherlands",
+    city: "Rotterdam",
+    description:
+      "45 minute tour to the Eramusbridge, Hotel New York and de Hoerenloper.",
+    details: "Bring a jacket, can be chilly!",
+    host: "Brian V",
+    dateFrom: "March 20 2020",
+    dateTo: "",
+    reviews: "🌮 7 🍻 3",
+  };
+
+  return (
+    <CardContainer>
+      <Card>
+        <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
+        <EventDetails>
+          <Title>{props.name}</Title>
+          <Location>
+            {props.country}, {props.city}
+          </Location>
+          <Description>{props.description}</Description>
+          <Details>{props.details}</Details>
+          <Row>
+            <Date>
+              {props.dateFrom}
+              {props.dateTo && " - " + props.dateTo}
+            </Date>
+            <Host>{props.host}</Host>
+          </Row>
+          <Reviews>{props.reviews}</Reviews>
+        </EventDetails>
+      </Card>
+    </CardContainer>
+  );
+}
--- screens/Listing.tsx
+++ screens/Listing.tsx
@@ -1,74 +1,63 @@
-import * as React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
-import {LIST_EVENTS} from '../graphql/queries';
-import { Text, View } from '../components/Themed';
-import { ApolloProvider, useQuery } from '@apollo/client';
-import { ActivityIndicator } from 'react-native-paper';
+import * as React from "react";
+import { FlatList, StyleSheet, ScrollView } from "react-native";
+import { LIST_EVENTS } from "../graphql/queries";
+import { Text, View } from "../components/Themed";
+import { ApolloProvider, useQuery } from "@apollo/client";
+import { ActivityIndicator } from "react-native-paper";
 
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import EventCard from "../components/Card";
 
 export default function Listing() {
-
-  const renderCard = ({item: listItem}:any) => {
-    return <Text>{listItem.name}</Text>
-  }
+  const renderCard = ({ item: listItem }: any) => {
+    return (
+      <EventCard name={listItem.name}/>
+    );
+  };
 
   const { loading, error, data } = useQuery(LIST_EVENTS, {
     // variables: { eventType: type },
   });
 
   const httpLink = createHttpLink({
-    uri: 'http://localhost:3000/graphql',
+    uri: "http://localhost:3000/graphql",
   });
-  
+
   const authLink = setContext((_, { headers }) => {
     // get the authentication token from local storage if it exists
-    const token = AsyncStorage.getItem('token');
+    const token = AsyncStorage.getItem("token");
     // return the headers to the context so httpLink can read them
     return {
       headers: {
         ...headers,
         authorization: token ? `Bearer ${token}` : "",
-      }
-    }
+      },
+    };
   });
-  
+
   const client = new ApolloClient({
     link: authLink.concat(httpLink),
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
   });
 
   return (
     <ApolloProvider client={client}>
-    <View style={styles.container}>
-      { loading && <ActivityIndicator/> }
-      { error && <Text>Error occured</Text> }
-      { data && !loading &&
-        <FlatList
-          data={data.allEvents}
-          renderItem={renderCard}
-        />
-      }
-    </View>
-  </ApolloProvider>
-  )
+      <ScrollView style={styles.container}>
+        {loading && <ActivityIndicator />}
+        {error && <Text>Error occured</Text>}
+        {data && !loading && (
+          <FlatList data={data.allEvents} renderItem={renderCard} />
+        )}
+      </ScrollView>
+    </ApolloProvider>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    padding: 20,
   },
 });
